2d43ffc64dd44bf78a409e62b43618bb9def0aa7
howker <howker2007@gmail.com>
2020-12-01 15:21:58 +0400

    all down

--- lib/main.dart
+++ lib/main.dart
@@ -17,11 +17,34 @@ class MyApp extends StatelessWidget {
 class MyFirstWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    runtypeReturn() {
+      return context.runtimeType;
+    }
+
+    return Container();
+  }
+}
+
+class MyStatefullWidget extends StatefulWidget {
+  @override
+  _MyStatefullWidgetState createState() => _MyStatefullWidgetState();
+}
+
+class _MyStatefullWidgetState extends State<MyStatefullWidget> {
+  @override
+  Widget build(BuildContext context) {
+    runtypeReturn() {
+      return context.runtimeType;
+    }
+
+    runtypeReturn();
     return Container();
   }
 }
 
 /* 
 1 - приложение не запустится т.к. отсутствует точка входа main 
-2 - 
+2 - значение title будет виден в верхней части в диспетчере задач, если нажать кнопку "последние приложения".
+3 - context.runtimeType не получилось реализовать в StatelessWidget, так как не на что ссылаться - 
+4 - context.runtimeType получилось реализовать в StatefullWidget 
 */
