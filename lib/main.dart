ebe200219437769933cb63296e8c11d62ad787d7
howker <howker2007@gmail.com>
2020-12-01 20:09:54 +0400

    function added

--- lib/main.dart
+++ lib/main.dart
@@ -15,12 +15,12 @@ class MyApp extends StatelessWidget {
 }
 
 class MyFirstWidget extends StatelessWidget {
+  runtypeReturn() {
+    return context.runtimeType;
+  }
+
   @override
   Widget build(BuildContext context) {
-    runtypeReturn() {
-      return context.runtimeType;
-    }
-
     return Container();
   }
 }
@@ -31,13 +31,12 @@ class MyStatefullWidget extends StatefulWidget {
 }
 
 class _MyStatefullWidgetState extends State<MyStatefullWidget> {
+  runtypeReturn() {
+    return context.runtimeType;
+  }
+
   @override
   Widget build(BuildContext context) {
-    runtypeReturn() {
-      return context.runtimeType;
-    }
-
-    runtypeReturn();
     return Container();
   }
 }
@@ -45,6 +44,6 @@ class _MyStatefullWidgetState extends State<MyStatefullWidget> {
 /* 
 1 - приложение не запустится т.к. отсутствует точка входа main 
 2 - значение title будет виден в верхней части в диспетчере задач, если нажать кнопку "последние приложения".
-3 - context.runtimeType не получилось реализовать в StatelessWidget, так как не на что ссылаться - 
-4 - context.runtimeType получилось реализовать в StatefullWidget 
+3 - context.runtimeType не получилось реализовать в StatelessWidget, так как нет контекста 
+4 - context.runtimeType получилось реализовать в StatefullWidget в части State, т.к здесь контекст уже есть 
 */
