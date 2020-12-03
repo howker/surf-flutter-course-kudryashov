c08866aa299b5408ab9d124a04ba7e246f8d3b7f
howker <howker2007@gmail.com>
2020-12-03 19:49:48 +0400

    Text and TextStyle were added

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -8,6 +8,23 @@ class SightListScreen extends StatefulWidget {
 class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 136,
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        title: Text(
+          'Список\nинтересных мест',
+          textAlign: TextAlign.left,
+          maxLines: 2,
+          style: TextStyle(
+            fontFamily: 'Roboto',
+            fontWeight: FontWeight.w700,
+            fontSize: 32,
+            color: Color(0xFF3B3E5B),
+          ),
+        ),
+      ),
+    );
   }
 }
