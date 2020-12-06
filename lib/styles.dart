7a7acb54f4c6195824dba11b6ec07c92240ed5af
howker <howker2007@gmail.com>
2020-12-06 15:24:23 +0400

    appbar

---
+++ lib/styles.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+const textStyle = TextStyle(
+  fontFamily: 'Roboto',
+  fontWeight: FontWeight.w700,
+  fontSize: 32,
+  color: Colors.green,
+);
+
+const textStyle1 = TextStyle(
+  fontFamily: 'Roboto',
+  fontWeight: FontWeight.w700,
+  fontSize: 32,
+  color: const Color(0xFF3B3E5B),
+);
+
+const textStyle2 = TextStyle(
+  fontFamily: 'Roboto',
+  fontWeight: FontWeight.w700,
+  fontSize: 32,
+  color: Colors.yellow,
+);
+
+const textStyle3 = TextStyle(
+  fontFamily: 'Roboto',
+  fontWeight: FontWeight.w700,
+  fontSize: 32,
+  color: const Color(0xFF3B3E5B),
+);
