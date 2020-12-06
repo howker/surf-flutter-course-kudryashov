720cea2cf62c53078cc08ec1832583324f13f76e
howker <howker2007@gmail.com>
2020-12-06 14:50:14 +0400

    mock data added

---
+++ lib/domain/sight.dart
@@ -0,0 +1,16 @@
+class Sight {
+  final String nameSights;
+  final double lat, lon;
+  final String url;
+  final String details;
+  final String type;
+
+  Sight({
+    this.nameSights,
+    this.lat,
+    this.lon,
+    this.url,
+    this.details,
+    this.type,
+  });
+}
---
+++ lib/mock.dart
@@ -0,0 +1,25 @@
+import 'package:places/domain/sight.dart';
+
+final List<Sight> mocks = [
+  Sight(
+    nameSights: 'Чёрная дыра',
+    lat: 38.858780,
+    lon: 111.603179,
+    url: 'https://joy-pup.com/wp-content/uploads/2019/03/hole_china.jpg',
+    details: 'Пугающая черная дыра в Китае',
+  ),
+  Sight(
+    nameSights: 'Гигантский смартфон в Омске',
+    lat: 55.015528,
+    lon: 73.425028,
+    url: 'https://joy-pup.com/wp-content/uploads/2019/03/smartphone_omsk.jpg',
+    details: 'Гигантский смартфон в Омске Российской Федерации',
+  ),
+  Sight(
+    nameSights: 'Кладбище кораблей',
+    lat: 51.285025,
+    lon: 30.212353,
+    url: 'https://joy-pup.com/wp-content/uploads/2019/03/ship_chernobil.jpg',
+    details: 'Кладбище кораблей в городе Чернобыль Киевской области, Украина',
+  ),
+];
