fe9b0dc832c609aed0df9a93a284a4b2ed8dc09e
howker <howker2007@gmail.com>
2020-12-16 20:23:12 +0400

    AspectRatio was added

--- lib/ui/screen/sight_card.dart
+++ lib/ui/screen/sight_card.dart
@@ -10,81 +10,88 @@ class SightCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Stack(
-        children: [
-          Container(
-            margin: EdgeInsets.only(left: 16, right: 16),
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.all(
-                Radius.circular(15),
-              ),
-            ),
-            height: 188,
-          ),
-          Positioned(
-            top: 96,
-            left: 16,
-            child: Container(
-              height: 92,
-              width: 380,
+      child: AspectRatio(
+        aspectRatio: 3 / 2,
+        child: Stack(
+          children: [
+            Container(
+              margin: EdgeInsets.only(left: 16, right: 16),
               decoration: BoxDecoration(
-                color: const Color(0xFFF5F5F5),
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(15),
-                  bottomRight: Radius.circular(15),
+                color: Colors.blue,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(15),
                 ),
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: 16,
+              height: 188,
+            ),
+            Positioned(
+              top: 96,
+              left: 16,
+              child: Container(
+                height: 92,
+                width: 380,
+                decoration: BoxDecoration(
+                  color: const Color(0xFFF5F5F5),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(15),
+                    bottomRight: Radius.circular(15),
                   ),
-                  Container(
-                    padding: EdgeInsets.only(
-                      left: 16,
-                      right: 16,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 16,
                     ),
-                    child: Text(
-                      sight.nameSights,
-                      style: textRegular16Black,
+                    Expanded(
+                      child: Container(
+                        padding: EdgeInsets.only(
+                          left: 16,
+                          right: 16,
+                        ),
+                        child: Text(
+                          sight.nameSights,
+                          style: textRegular16Black,
+                        ),
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 2,
-                  ),
-                  Container(
-                    padding: EdgeInsets.only(
-                      left: 16,
-                      right: 16,
+                    SizedBox(
+                      height: 2,
                     ),
-                    child: Text(
-                      sight.details,
-                      style: textRegular14Grey,
+                    Expanded(
+                      child: Container(
+                        padding: EdgeInsets.only(
+                          left: 16,
+                          right: 16,
+                        ),
+                        child: Text(
+                          sight.details,
+                          style: textRegular14Grey,
+                        ),
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-          Positioned(
+            Positioned(
+                top: 19,
+                left: 36,
+                child: Text(
+                  sight.type,
+                  style: textBold14Black.copyWith(color: Colors.white),
+                )),
+            Positioned(
               top: 19,
-              left: 36,
-              child: Text(
-                sight.type,
-                style: textBold14Black.copyWith(color: Colors.white),
-              )),
-          Positioned(
-            top: 19,
-            right: 36,
-            child: Container(
-              height: 18,
-              width: 20,
-              color: Colors.white,
+              right: 36,
+              child: Container(
+                height: 18,
+                width: 20,
+                color: Colors.white,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
