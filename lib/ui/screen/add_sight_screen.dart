790ce1ca380112aa5dcb0282f9e7d5c4693c3ba4
howker <howker2007@gmail.com>
2021-03-26 08:26:55 +0400

    merge confl resolved

--- lib/ui/screen/add_sight_screen.dart
+++ lib/ui/screen/add_sight_screen.dart
@@ -247,14 +247,8 @@ class _AddSightScreenState extends State<AddSightScreen> {
                   },
                   color: Theme.of(context).primaryColorDark,
                 ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.green, width: 1.0),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.green, width: 1.0),
-                  borderRadius: BorderRadius.circular(8),
-                ),
+                enabledBorder: _outlineInputBorder(),
+                focusedBorder: _outlineInputBorder(),
               ),
               onEditingComplete: () {
                 focusNodeNewPlaceScreenDescription.requestFocus();
@@ -266,6 +260,13 @@ class _AddSightScreenState extends State<AddSightScreen> {
           ),
         ),
       ],
+    );
+  }
+
+  OutlineInputBorder _outlineInputBorder() {
+    return OutlineInputBorder(
+      borderSide: const BorderSide(color: Colors.green, width: 1.0),
+      borderRadius: BorderRadius.circular(8),
     );
   }
 
