e0832464b2f2c69aa3511d0831580d175f0caeb1
howker <howker2007@gmail.com>
2021-03-30 12:16:15 +0400

    photo cards added

--- lib/mock.dart
+++ lib/mock.dart
@@ -83,3 +83,57 @@ const double userLat = 46.348838;
 const double userLng = 48.029816;
 
 List<Sight> sortedByRadius = [];
+
+final List<Sight> mocksPhotos = [
+  Sight(
+      nameSights: 'Чёрная дыра',
+      lat: 38.858780,
+      lon: 111.603179,
+      url: 'https://joy-pup.com/wp-content/uploads/2019/03/hole_china.jpg',
+      details: 'Пугающая черная дыра в Китае',
+      type: 'необычное место'),
+  Sight(
+      nameSights: 'Гигантский смартфон в Омске',
+      lat: 55.015528,
+      lon: 73.425028,
+      url: 'https://joy-pup.com/wp-content/uploads/2019/03/smartphone_omsk.jpg',
+      details: 'Гигантский смартфон в Омске Российской Федерации',
+      type: 'необычное место'),
+  Sight(
+      nameSights: 'Кладбище кораблей',
+      lat: 51.285025,
+      lon: 30.212353,
+      url: 'https://joy-pup.com/wp-content/uploads/2019/03/ship_chernobil.jpg',
+      details: 'Кладбище кораблей в городе Чернобыль Киевской области, Украина',
+      type: 'необычное место'),
+  Sight(
+      nameSights: 'Астраханский кремль',
+      lat: 46.349540,
+      lon: 48.030772,
+      url: 'https://cdn.photosight.ru/img/8/2a2/6386165_large.jpg',
+      details: 'Астраханский кремль',
+      type: 'Музей'),
+  Sight(
+      nameSights: 'Ресторан  Белуга',
+      lat: 46.354357,
+      lon: 48.033053,
+      url:
+          'https://i1.photo.2gis.com/images/branch/8/1125899943872413_ffb7.jpg',
+      details: 'славится своими рыбными блюдами',
+      type: 'Ресторан'),
+  Sight(
+      nameSights: 'Ресторан  Пряности',
+      lat: 46.352273,
+      lon: 48.033619,
+      url: 'https://fb.ru/misc/i/gallery/43527/3214514.jpg',
+      details: 'Кухня: европейская, итальянская, японская',
+      type: 'Ресторан'),
+  Sight(
+      nameSights: 'Ресторан  Брюнетка и браконьер',
+      lat: 46.349953,
+      lon: 48.041677,
+      url:
+          'https://avatars.mds.yandex.net/get-altay/1899727/2a0000016b902917279fc4dea94440130f4b/XXXL',
+      details: 'Кухня:европейская, авторская, смешанная',
+      type: 'Ресторан'),
+];
--- lib/ui/screen/add_sight_screen.dart
+++ lib/ui/screen/add_sight_screen.dart
@@ -41,56 +41,202 @@ class _AddSightScreenState extends State<AddSightScreen> {
       appBar: _buildAppBar(context),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 40),
-            Text(
-              newPlaceScreenCategory,
-              style: Theme.of(context).textTheme.bodyText1.copyWith(
-                    fontSize: 12,
-                    color: lmInactiveBlackColor,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 40),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: _buildPhotoCardList(),
+                ),
+              ),
+              const SizedBox(height: 24),
+              Text(
+                newPlaceScreenCategory,
+                style: Theme.of(context).textTheme.bodyText1.copyWith(
+                      fontSize: 12,
+                      color: lmInactiveBlackColor,
+                    ),
+              ),
+              const SizedBox(height: 14),
+              _buildCategoryFrame(context),
+              const Divider(thickness: 0.8),
+              const SizedBox(height: 24),
+              Text(
+                newPlaceScreenName,
+                style: Theme.of(context).textTheme.bodyText1.copyWith(
+                      fontSize: 12,
+                      color: lmInactiveBlackColor,
+                    ),
+              ),
+              const SizedBox(height: 12),
+              _buildNameFrame(context),
+              const SizedBox(height: 24),
+              _buildLatLonLabels(context),
+              const SizedBox(height: 12),
+              _buildLatLonFrame(context),
+              const SizedBox(height: 15),
+              Text(
+                newPlaceScreenPointAtMap,
+                style: Theme.of(context)
+                    .textTheme
+                    .headline5
+                    .copyWith(color: Colors.green),
+              ),
+              const SizedBox(height: 37),
+              Text(
+                newPlaceScreenDescription,
+                style: Theme.of(context).textTheme.bodyText1.copyWith(
+                      fontSize: 12,
+                      color: lmInactiveBlackColor,
+                    ),
+              ),
+              const SizedBox(height: 12),
+              _buildDescriptionFrame(context),
+              const SizedBox(height: 58),
+              _buildCreateButton(context),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildAppBar(BuildContext context) {
+    return AppBar(
+      leadingWidth: 80,
+      backgroundColor: Theme.of(context).primaryColor,
+      title: Text(
+        newPlaceScreenTitle,
+        style: Theme.of(context).textTheme.headline1,
+      ),
+      elevation: 0,
+      centerTitle: true,
+      leading: _buildCancelButton(context),
+    );
+  }
+
+  List<Widget> _buildPhotoCardList() {
+    List<Widget> photoCardList = [
+      Padding(
+        padding: const EdgeInsets.only(right: 16),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            border: Border.all(
+              color: Colors.green,
+              width: 2,
+            ),
+          ),
+          height: 72,
+          width: 72,
+          child: const Icon(
+            Icons.add,
+            color: Colors.green,
+            size: 34,
+          ),
+        ),
+      ),
+    ];
+
+    for (var element in mocksPhotos) {
+      photoCardList.add(
+        Padding(
+          padding: const EdgeInsets.only(right: 16),
+          child: Stack(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: Container(
+                  height: 72,
+                  width: 72,
+                  child: Image.network(
+                    element.url,
+                    fit: BoxFit.cover,
+                    loadingBuilder: (BuildContext context, Widget child,
+                        ImageChunkEvent loadingProgress) {
+                      if (loadingProgress == null) return child;
+                      return Center(
+                        child: CircularProgressIndicator(
+                          value: loadingProgress.expectedTotalBytes != null
+                              ? loadingProgress.cumulativeBytesLoaded /
+                                  loadingProgress.expectedTotalBytes
+                              : null,
+                        ),
+                      );
+                    },
                   ),
-            ),
-            const SizedBox(height: 14),
-            _buildCategoryFrame(context),
-            const Divider(thickness: 0.8),
-            const SizedBox(height: 24),
-            Text(
-              newPlaceScreenName,
-              style: Theme.of(context).textTheme.bodyText1.copyWith(
-                    fontSize: 12,
-                    color: lmInactiveBlackColor,
-                  ),
-            ),
-            const SizedBox(height: 12),
-            _buildNameFrame(context),
-            const SizedBox(height: 24),
-            _buildLatLonLabels(context),
-            const SizedBox(height: 12),
-            _buildLatLonFrame(context),
-            const SizedBox(height: 15),
-            Text(
-              newPlaceScreenPointAtMap,
-              style: Theme.of(context)
-                  .textTheme
-                  .headline5
-                  .copyWith(color: Colors.green),
-            ),
-            const SizedBox(height: 37),
-            Text(
-              newPlaceScreenDescription,
-              style: Theme.of(context).textTheme.bodyText1.copyWith(
-                    fontSize: 12,
-                    color: lmInactiveBlackColor,
-                  ),
-            ),
-            const SizedBox(height: 12),
-            _buildDescriptionFrame(context),
-            const SizedBox(height: 58),
-            _buildCreateButton(context),
-          ],
+                ),
+              ),
+              Positioned(
+                right: 5,
+                top: 5,
+                child: GestureDetector(
+                  child: const Icon(Icons.cancel),
+                  onTap: () {
+                    setState(() {
+                      mocksPhotos.remove(element);
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+    return photoCardList;
+  }
+
+  Widget _buildCategoryFrame(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          newPlaceCategory,
+          style: Theme.of(context)
+              .textTheme
+              .caption
+              .copyWith(color: lmInactiveBlackColor),
+        ),
+        IconButton(
+          icon: Icon(
+            Icons.navigate_next,
+            color: Theme.of(context).primaryColorDark,
+          ),
+          onPressed: () {
+            _returnNewPlaceCategory(context);
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget _buildNameFrame(BuildContext context) {
+    return Container(
+      height: 40,
+      width: double.infinity,
+      child: TextField(
+        focusNode: focusNodeNewPlaceScreenName,
+        textCapitalization: TextCapitalization.sentences,
+        style: Theme.of(context).textTheme.caption,
+        cursorWidth: 1,
+        cursorHeight: 24,
+        onChanged: (String value) {
+          nameSight = value;
+        },
+        onEditingComplete: () {
+          focusNodeNewPlaceScreenLat.requestFocus();
+        },
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.all(10),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.green, width: 1.0),
+            borderRadius: BorderRadius.circular(8),
+          ),
         ),
       ),
     );
@@ -116,78 +262,6 @@ class _AddSightScreenState extends State<AddSightScreen> {
               .copyWith(fontSize: 12, color: lmInactiveBlackColor),
         ),
       ],
-    );
-  }
-
-  Widget _buildAppBar(BuildContext context) {
-    return AppBar(
-      leadingWidth: 80,
-      backgroundColor: Theme.of(context).primaryColor,
-      title: Text(
-        newPlaceScreenTitle,
-        style: Theme.of(context).textTheme.headline1,
-      ),
-      elevation: 0,
-      centerTitle: true,
-      leading: _buildCancelButton(context),
-    );
-  }
-
-  Widget _buildCreateButton(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 8),
-      child: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          minimumSize: Size(double.infinity, 48),
-        ),
-        onPressed: () {
-          Sight newSight = Sight(
-            nameSights: nameSight,
-            lat: latSight,
-            lon: lonSight,
-            url: urlSight,
-            details: detailsSight,
-            type: typeSight,
-          );
-          mocks.add(newSight);
-        },
-        child: Text(
-          newPlaceScreenCreateButton,
-          style: Theme.of(context).textTheme.bodyText2,
-        ),
-      ),
-    );
-  }
-
-  Widget _buildDescriptionFrame(BuildContext context) {
-    return Container(
-      height: 80,
-      width: double.infinity,
-      child: TextField(
-        textCapitalization: TextCapitalization.sentences,
-        focusNode: focusNodeNewPlaceScreenDescription,
-        style: Theme.of(context).textTheme.caption,
-        cursorWidth: 1,
-        cursorHeight: 24,
-        decoration: InputDecoration(
-          contentPadding: const EdgeInsets.only(top: 10, left: 16, bottom: 80),
-          hintText: newPlaceScreenInputText,
-          hintStyle: Theme.of(context)
-              .textTheme
-              .caption
-              .copyWith(color: lmInactiveBlackColor),
-          enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.green, width: 1.0),
-            borderRadius: BorderRadius.circular(8),
-          ),
-        ),
-        onEditingComplete: () {
-          focusNodeNewPlaceScreenDescription.unfocus();
-        },
-        onChanged: (String value) {
-          detailsSight = value;
-        },
-      ),
     );
   }
 
@@ -263,61 +337,68 @@ class _AddSightScreenState extends State<AddSightScreen> {
     );
   }
 
-  OutlineInputBorder _outlineInputBorder() {
-    return OutlineInputBorder(
-      borderSide: const BorderSide(color: Colors.green, width: 1.0),
-      borderRadius: BorderRadius.circular(8),
-    );
-  }
-
-  Widget _buildNameFrame(BuildContext context) {
+  Widget _buildDescriptionFrame(BuildContext context) {
     return Container(
-      height: 40,
+      height: 80,
       width: double.infinity,
       child: TextField(
-        focusNode: focusNodeNewPlaceScreenName,
         textCapitalization: TextCapitalization.sentences,
+        focusNode: focusNodeNewPlaceScreenDescription,
         style: Theme.of(context).textTheme.caption,
         cursorWidth: 1,
         cursorHeight: 24,
-        onChanged: (String value) {
-          nameSight = value;
-        },
-        onEditingComplete: () {
-          focusNodeNewPlaceScreenLat.requestFocus();
-        },
         decoration: InputDecoration(
-          contentPadding: const EdgeInsets.all(10),
+          contentPadding: const EdgeInsets.only(top: 10, left: 16, bottom: 80),
+          hintText: newPlaceScreenInputText,
+          hintStyle: Theme.of(context)
+              .textTheme
+              .caption
+              .copyWith(color: lmInactiveBlackColor),
           enabledBorder: OutlineInputBorder(
             borderSide: BorderSide(color: Colors.green, width: 1.0),
             borderRadius: BorderRadius.circular(8),
           ),
         ),
+        onEditingComplete: () {
+          focusNodeNewPlaceScreenDescription.unfocus();
+        },
+        onChanged: (String value) {
+          detailsSight = value;
+        },
       ),
     );
   }
 
-  Widget _buildCategoryFrame(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(
-          newPlaceCategory,
-          style: Theme.of(context)
-              .textTheme
-              .caption
-              .copyWith(color: lmInactiveBlackColor),
+  Widget _buildCreateButton(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          minimumSize: Size(double.infinity, 48),
         ),
-        IconButton(
-          icon: Icon(
-            Icons.navigate_next,
-            color: Theme.of(context).primaryColorDark,
-          ),
-          onPressed: () {
-            _returnNewPlaceCategory(context);
-          },
+        onPressed: () {
+          Sight newSight = Sight(
+            nameSights: nameSight,
+            lat: latSight,
+            lon: lonSight,
+            url: urlSight,
+            details: detailsSight,
+            type: typeSight,
+          );
+          mocks.add(newSight);
+        },
+        child: Text(
+          newPlaceScreenCreateButton,
+          style: Theme.of(context).textTheme.bodyText2,
         ),
-      ],
+      ),
+    );
+  }
+
+  OutlineInputBorder _outlineInputBorder() {
+    return OutlineInputBorder(
+      borderSide: const BorderSide(color: Colors.green, width: 1.0),
+      borderRadius: BorderRadius.circular(8),
     );
   }
 
