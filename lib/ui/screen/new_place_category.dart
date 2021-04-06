5b3f85bb124ecd16c4ea76630f1007d4235a367a
howker <howker2007@gmail.com>
2021-04-06 10:55:03 +0400

    category fix

--- lib/ui/screen/new_place_category.dart
+++ lib/ui/screen/new_place_category.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:places/mock.dart';
 import 'package:places/text_string_const.dart';
@@ -16,16 +18,46 @@ class _NewPlaceCategoryScreenState extends State<NewPlaceCategoryScreen> {
       appBar: _buildAppBar(context),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Column(
-              children: _buildCheckboxListTile(context),
-            ),
-            _buildSaveButton(context),
-          ],
+        child: ListView.separated(
+          itemBuilder: (BuildContext context, int index) {
+            return CheckboxListTile(
+              activeColor: Theme.of(context).primaryColor,
+              checkColor: Colors.green,
+              title: Text(
+                placesCategory[index].key,
+                style: Theme.of(context).textTheme.caption,
+              ),
+              value: placesCategory[index].value,
+              onChanged: (bool newValue) {
+                setState(() {
+                  //placesCategory.updateAll((placesCategory[index].key, placesCategory[index].value) => false);
+                  placesCategory.update(
+                      placesCategory[index].key, (value) => newValue);
+                  newPlaceCategory = placesCategory[index].key;
+                });
+              },
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) {
+            return const Divider();
+          },
+          itemCount: placesCategory.length,
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          physics: Platform.isAndroid
+              ? ClampingScrollPhysics()
+              : BouncingScrollPhysics(),
         ),
+
+        // Column(
+        //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //   crossAxisAlignment: CrossAxisAlignment.start,
+        //   children: [
+        //     Column(
+        //       children: _buildCheckboxListTile(context),
+        //     ),
+        //     _buildSaveButton(context),
+        //   ],
+        // ),
       ),
     );
   }
