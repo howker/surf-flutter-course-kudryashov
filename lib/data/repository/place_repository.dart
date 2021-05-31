42dcb347c7a2e6236d3cd7a8ee084cc9ea1edc7a
howker <howker2007@gmail.com>
2021-05-31 08:18:22 +0400

    dio fixed

--- lib/data/repository/place_repository.dart
+++ lib/data/repository/place_repository.dart
@@ -2,15 +2,13 @@ import 'dart:convert';
 
 import 'package:places/data/model/place.dart';
 import 'package:places/data/model/places_filter_request_dto.dart';
-import 'package:places/api_service.dart';
+import 'package:places/dio.dart';
 
 ///Репозиторий с запросами к api
 class PlaceRepository {
-  final Api api = Api();
-
   Future<List<Place>> getPlaces() async {
-    final response = await api.get(ApiConsts.places);
-    return jsonDecode(response)
+    final response = await dio.get(ApiConsts.places);
+    return jsonDecode(response.data)
         .data
         .map((response) => Place.fromJson(response))
         .toList();
@@ -18,35 +16,35 @@ class PlaceRepository {
 
   Future<List<Place>> getFilteredPlaces(PlacesFilterRequestDto filter) async {
     final response =
-        await api.post(ApiConsts.getFilteredPlaces, filter.toJson());
-    return jsonDecode(response)
+        await dio.post(ApiConsts.getFilteredPlaces, data: filter.toJson());
+    return jsonDecode(response.data)
         .data
         .map((response) => Place.fromJson(response))
         .toList();
   }
 
   Future<Place> createPlace(Place place) async {
-    final response = await api.post(ApiConsts.places, place.toJson());
-    return jsonDecode(response)
+    final response = await dio.post(ApiConsts.places, data: place.toJson());
+    return jsonDecode(response.data)
         .data
         .map((response) => Place.fromJson(response))
         .toList();
   }
 
   Future<Place> getPlaceById(int id) async {
-    final response = await api.get(ApiConsts.places + '/$id');
-    return jsonDecode(response)
+    final response = await dio.get(ApiConsts.places + '/$id');
+    return jsonDecode(response.data)
         .data
         .map((response) => Place.fromJson(response))
         .toList();
   }
 
   Future<void> deletePlaceById(int id) async {
-    await api.delete(ApiConsts.places + '/$id');
+    await dio.delete(ApiConsts.places + '/$id');
   }
 
   Future<void> updatePlace(Place place) async {
-    await api.put(ApiConsts.places + '${place.id}', place.toJson());
+    await dio.put(ApiConsts.places + '${place.id}', data: place.toJson());
   }
 }
 
